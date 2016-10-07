bbf033248d8250ff324aed09019fb70bcf33fed4
yuriykuzin <yuriy.kuzin@gmail.com>
2016-10-07 06:58:38 +0000

    Upd webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+'use strict';
+
+const MODE = process.env.MODE || 'dev';
+
 var GhPagesWebpackPlugin = require('gh-pages-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './xo-game-main.js',
@@ -8,17 +13,17 @@ module.exports = {
     filename: 'xo-game-bundle.js'
   },
 
+  watch: MODE === 'dev',
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: MODE === 'dev' ? 'source-map' : null,
+
   plugins: [
-/*    new GhPagesWebpackPlugin({
-      path: './dist',
-      options: {
-        message: 'Update live page by webpack',
-        user: {
-          name: 'Yuriy Kuzin',
-          email: ''
-        }
-      }
-    }),*/
+    new webpack.NoErrorsPlugin(),
+    
     new CopyWebpackPlugin([{
       from: 'index.html',
       to: ''
@@ -29,3 +34,27 @@ module.exports = {
   ]
 
 };
+
+if (MODE === 'prod') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    }));
+
+  module.exports.plugins.push(
+    new GhPagesWebpackPlugin({
+      path: './dist',
+      options: {
+        message: 'Update live page by webpack',
+        user: {
+          name: 'Yuriy Kuzin',
+          email: ''
+        }
+      }
+    })
+  );
+}
